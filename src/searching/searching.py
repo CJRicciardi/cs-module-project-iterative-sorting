8a64ca2c90f361c31804c8373e762d5d7051a83e
CJ Ricciardi <ricciardistg@gmail.com>
2020-06-16 14:56:58 -0400

    Homework complete.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,7 @@
 def linear_search(arr, target):
-    # Your code here
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -7,8 +9,32 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
 
-    # Your code here
+        for j in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
+
+    l = len(arr)
+    low = 0
+    high = l - 1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if arr[mid] < target:
+            low = mid + 1
+
+        elif arr[mid] > target:
+            high = mid - 1
+
+        else:
+            return mid
+        
 
     return -1  # not found
